dc47fd6831b9ed79a6cf9d4399b90ea86fcd7833
FlexaDela <mateusflexa2000@gmail.com>
2023-10-31 14:09:44 -0300

    Criando o "models" de "tarefas"
    Criei a parte de models no app de tarefas, criando a descrição, criação,categoria e status. Ou seja o modelo é para descrever o esquema de dados de uma aplicação.

--- sgt/tarefas/models.py
+++ sgt/tarefas/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+#descrição 
+#data de criação
+#categoria(urgente,importante,precisa ser feito)
+ #statuss(concluido,pendente,adiado)
 
-# Create your models here.
+class Tarefas(models.Model):
+    OPCOES_STATUS =(
+        ('concluído','Concluido'),
+        ('pendente','Pendente'),
+        ('Adiado','Adiado'),
+    )
+
+    OPCOES_CATEGORIAS =(
+        ('urgente','Urgente'),
+        ('importante','Importante'),
+        ('precisa ser feito','Precisa ser feito'),
+    )
+
+    descricao = models.CharField(max_length=400)
+    criacao = models.DateTimeField(auto_now_add=True)
+    categorias = models.CharField(max_length=25, choices = OPCOES_CATEGORIAS,
+                                  default='importante')
+    status = models.Charfield(max_length=25, choices = OPCOES_STATUS,
+                              default= 'pendente')
+
+
